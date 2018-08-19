06e9652b66fc23c39bbfb653f4bd6c045fa41be4
Jen Strong <jenniferastrong@gmail.com>
2018-08-19 11:41:21 +0000

    Update welcome message in app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const helloWorld = 'Welcome to React';
+    const name = {
+      first: 'Jane',
+      last: 'Smith'
+    }
     return (
       <div className="App">
-          <h2>Welcome to React</h2>
+          <h2>{helloWorld}, {name.first} {name.last}</h2>
       </div>
     );
   }
